55bb8eae6d8e60338a9ed43f8fe04cb2a1107969
joyran <xinshuiyihan@126.com>
2020-03-07 14:20:01 +0800

    chore: update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,5 @@ module.exports = {
     port: 8027,
     disableHostCheck: true // 解决127.0.0.1指向其他域名时出现"Invalid Host header"问题
   },
-  productionSourceMap: false,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/bot/dist/'
-    : '/'
+  productionSourceMap: false
 }
